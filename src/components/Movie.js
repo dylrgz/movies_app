4a8b1ad39f0c4aa60ae834fa17a05c835f0a434c
Murat Koçak <murat33kocak@gmail.com>
2022-10-17 13:14:41 +0200

    Finished?

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,28 @@
 import React from "react";
 const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
-const Movie = ({ title, poster_path, overview, vote_average }) => (
+const setVoteClass = (vote)=>{
+    if(vote>=8){
+        return 'green'
+    }
+    else if(vote>=6){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
+
+const Movie = ({ title, poster_path, overview, release_date, vote_average }) => (
     <div className="movie">
-        <img src={IMG_API + poster_path} alt={title} />
+        <img src={ poster_path?(IMG_API + poster_path) : 'https://avatars.mds.yandex.net/i?id=ceca5db3992d8cf5111118f3cbfb083e-4376151-images-thumbs&n=13&exp=1'} alt={title} />
         <div className="movie-info">
-            <h3>{title}</h3>
-            <span>{vote_average}</span>
+            <h3>{title + "   ("+release_date.substring(0,4)+")"}</h3>
+            <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>
         </div>
 
         <div className="movie-over">
-            <h2>OverView:</h2>
+            <h2>Overview:</h2>
             <p>{overview}</p>
         </div>
     </div>
